bc1d4e5667c63f75beee9419858324913b957f08
Pablo Damian Berdun <pablodamianberdun@gmail.com>
2021-04-01 12:38:22 -0300

    If urlImage or description are null, don't render NewsCard

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import { Card } from 'antd';
-import styles from "../styles/newsCard.module.css"
+import React, { Fragment } from "react";
+import { Card } from "antd";
+import styles from "../styles/newsCard.module.css";
 
 const { Meta } = Card;
 
 const NewsCard = ({ news }) => {
-	const { title, urlToImage, description } = news
+    const { title, urlToImage, description } = news;
 
-	return ( 
-		<Card
-			className={styles.card}
-			cover={
-			<img
-				alt="newsImage"
-				src={urlToImage}
-			/>
-			}
-		>
-			<Meta
-				title={title}
-				description={description}
-			/>
-		</Card>
-	);
-}
- 
+    return (
+        <Fragment>
+            {urlToImage || description ? (
+                <Card
+                    className={styles.card}
+                    cover={<img alt="newsImage" src={urlToImage} />}
+                >
+                    <Meta title={title} description={description} />
+                </Card>
+            ) : null}
+        </Fragment>
+    );
+};
+
 export default NewsCard;
